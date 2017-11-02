21ab55545455516c91c1c5e1e9be270dece7faec
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2017-11-02 14:05:35 +0100

    Update GuessNumberOneTime.java

--- GuessNumberOneTime.java
+++ GuessNumberOneTime.java
@@ -12,21 +12,24 @@ public class GuessNumberOneTime {
     // Generate a random number to be guessed
     int number = (int)(Math.random() * 101);
     int guess=0;
+    //Input objekt, så vi kan tage et input
     Scanner input = new Scanner(System.in);
-    
+    //udskriver 
     System.out.println("Guess a magic number between 0 and 100");
+    //While løkke, som kører så længe guess er anderledes end number
     while(guess !=number)
     {
     // Prompt the user to guess the number
     System.out.print("\nEnter your guess: ");
+    //Gemmer indtastningen som guess
     guess = input.nextInt();
       
-    if (guess == number)
-      System.out.println("Yes, the number is " + number);
-    else if (guess > number)
-      System.out.println("Your guess is too high");
-    else 
-      System.out.println("Your guess is too low");    
+    if (guess == number)//Hvis guess er det samme number
+      System.out.println("Yes, the number is " + number); //Udskriver med nummeret
+    else if (guess > number)//Hvis guess er større end number
+      System.out.println("Your guess is too high");//Udskriver
+    else //Ellers
+      System.out.println("Your guess is too low"); //Udskriver   
     }
   }
 }
